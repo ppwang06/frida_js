c7c138ae2f499500efd016d22ac788864aacb66b
ppwang <ppwang06@163.com>
2023-02-23 21:46:19 +0800

    [add] add 不同模板

---
+++ moban.js
@@ -0,0 +1,18 @@
+
+console.log("frida start into app ...")
+
+//frida -U --no-pause -f com.shizhuang.duapp -l dewu.js
+
+
+Java.perform(function dewu() {
+    console.log("Frida start to update  method")
+    let head = Java.use("com.shizhuang.dudatastatistics.aliyunsls.DataStatistics")
+
+    head.t.overload('java.lang.String').implementation = function (str) {
+        console.log("传入字符串为:", str)
+        let result = this.t(str);
+        console.log("md5后数据为：", result)
+        return result
+    }
+
+})

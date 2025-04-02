31ca09b1f2ce7d6d156f557f7a612d2be441c5dc
wangpengpeng <ppwang06@163.com>
2025-04-02 15:58:35 +0800

    [fixed] update3

--- apps/taobao.js
+++ apps/taobao.js
@@ -11,13 +11,17 @@ adb forward tcp:27043 tcp:27043
 frida -U -f com.taobao.taobao  -l taobao.js
 frida -H 127.0.0.1:8080 -f com.taobao.taobao  -l taobao.js
 frida -H 127.0.0.1:8080 -f com.tmall.wireless  -l taobao.js
+
+-l 127.0.0.1:8083
+adb forward tcp:8083 tcp:8083
+frida -H 127.0.0.1:8083 -f com.taobao.taobao -l taobao.js
 * */
 Java.perform(function () {
     var enableSpdy = false;
     var SwitchConfig = Java.use('mtopsdk.mtop.global.SwitchConfig');
 
     var instance = SwitchConfig.getInstance();
-    instance.setGlobalSpdySslSwitchOpen(enableSpdy);
+    // instance.setGlobalSpdySslSwitchOpen(enableSpdy);
     instance.setGlobalSpdySwitchOpen(enableSpdy);
 
 });
